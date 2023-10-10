2f29fcc8d549286077165d94f82d4f300958d876
YounessJervi <jervijeff@gmail.com>
2023-10-10 16:39:20 -0700

    yes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * prints every minute of the day
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
+
 void jack_bauer(void)
 {
 	int i, j;
