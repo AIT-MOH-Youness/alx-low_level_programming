bb3c7c3a683e568762e6c1c746ee5bbe485af66b
YounessJervi <jervijeff@gmail.com>
2023-10-17 15:49:19 -0700

    g

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,6 @@
 /**
  * print_rev - imprime en reversa
  * @s: string
- * @str: string
  * return: 0
  */
 void print_rev(char *s)
@@ -10,12 +9,13 @@ void print_rev(char *s)
     int cpt=0;
     while(*s != '\0'){
         cpt++;
+        s++;
     }
-    char str[cpt];
-    for(int i=0; i<=cpt; i++){
-        str[i]=s[cpt-i];
+    
+    for( int i=cpt ; i >=0; i--){
+        _putchar(*s);
+        s--
     }
 
-    return(str);
 }
 
