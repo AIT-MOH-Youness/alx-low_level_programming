4ebdd0fe21bd5f38701bc7899b920e77c9b3f244
Youness-AIT-MOH <jervijeff@gmail.com>
2023-10-30 14:03:25 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
  * main - the main function
- *
+ * putchar - used to print
  * Return: Always 1 (Success)
 */
 int main(void)
 {
-char exp[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ;
-for(int i=0;i<59;i++)
-{
-putchar(exp[i]);	
-}
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
