c5f2d7639f5272c20841fc8c095b3d4b7b900b8b
Youness-AIT-MOH <jervijeff@gmail.com>
2023-10-29 17:40:24 -0700

    programming time

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * this code print a message as string in the console
  * Return - always return 0 (Successful)
 */
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
-}	
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
