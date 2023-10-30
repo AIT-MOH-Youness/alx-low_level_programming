5dd33b06d39b8669d4557b765b91a77ca40a6f47
Youness-AIT-MOH <jervijeff@gmail.com>
2023-10-29 17:37:27 -0700

    programming time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the entry function for the file
+ * this code print a message as string in the console
+ * Return - always return 0 (Successful)
+*/
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}	
