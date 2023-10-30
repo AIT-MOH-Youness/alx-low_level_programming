df393b4a14c416226e964a13f70a39f87270d937
Youness-AIT-MOH <jervijeff@gmail.com>
2023-10-30 13:27:11 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: Print alphabet lowercase but e and q
+ * Return: Zero
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
